c07fd5b5532bb598512703aa5dcf32a8af398a5d
B45i <amalshajan2011@gmail.com>
2020-07-18 20:29:43 +0530

    post method

--- src/App.js
+++ src/App.js
@@ -30,6 +30,17 @@ class App extends Component {
   postData() {
     // TODO: send to server after debounce
     console.log(this.state);
+    fetch('https://esp.local', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
   }
 
   render() {
