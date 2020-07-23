992f824c8b779b4fc153aee2fc293cc4ccb04e49
yedu-kn <yedukn@gmail.com>
2020-07-23 18:07:56 +0530

    Updates

---
+++ precache-manifest.e35954c2129e5968de92812d302e9dea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7063ac946c88612db16a0e306fa89d3e",
+    "url": "/Smart-Bulb/index.html"
+  },
+  {
+    "revision": "1227a3c57156e6133933",
+    "url": "/Smart-Bulb/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "db2f0db0a9dbe2aaa868",
+    "url": "/Smart-Bulb/static/css/main.af99092d.chunk.css"
+  },
+  {
+    "revision": "1227a3c57156e6133933",
+    "url": "/Smart-Bulb/static/js/2.42d69e78.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/Smart-Bulb/static/js/2.42d69e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2f0db0a9dbe2aaa868",
+    "url": "/Smart-Bulb/static/js/main.90ac7b32.chunk.js"
+  },
+  {
+    "revision": "60c12f7735d2073a2a6b",
+    "url": "/Smart-Bulb/static/js/runtime-main.d123f186.js"
+  }
+]);
